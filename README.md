d5e030cf7a0c1bfc8902c80f82f89e8adea9962d
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-16 21:17:43 +0100

    firebase Auth for React App

---
+++ README.md
@@ -0,0 +1,6 @@
+# Source Code for the React Firebase project.
+
+# TFK cumputer sience compte firebase
+It's using React Router v6 and Firebase v9.
+
+You like it ? Star it !
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Navbar from './components/Navbar'
+import SignUpModal from "./components/SignUpModal";
+import SignInModal from "./components/SignInModal"
+import Private from "./pages/Private/Private"
+import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
+
+function App() {
+  return (
+    <>
+      <SignUpModal />
+      <SignInModal />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<Private />}>
+          <Route path="/private/private-home" element={<PrivateHome />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
